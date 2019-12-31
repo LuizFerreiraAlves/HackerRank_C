6ca765069cd27c6f774f601b03b5e403e899f490
Luiz Ferreira Alves <37409686+LuizFerreiraAlves@users.noreply.github.com>
2019-12-31 15:14:39 -0300

    Update Permutations_of_Strings.c

--- Solutions/Permutations_of_Strings.c
+++ Solutions/Permutations_of_Strings.c
@@ -4,20 +4,22 @@
 
 int next_permutation(int n, char **s)
 {
-	/**
-	* Complete this method
-	* Return 0 when there is no next permutation and 1 otherwise
-	* Modify array s to its next permutation
-	*/
+    /**
+    * Complete this method
+    * Return 0 when there is no next permutation and 1 otherwise
+    * Modify array s to its next permutation
+    */
     int i = n-1;
     while(i>0 && strcmp(s[i-1],s[i])>=0) 
         i--;    // find key
+	
     if (i<=0) return 0;
     
     // Swap key with its successor in suffix
     int j = n-1;
     while(strcmp(s[i-1],s[j])>=0) 
         j--;    // find rightmost successor to key
+	
     char *tmp = s[i-1];
     s[i-1] = s[j];
     s[j] = tmp;
@@ -42,14 +44,15 @@ int main()
 	s = calloc(n, sizeof(char*));
 	for (int i = 0; i < n; i++)
 	{
-		s[i] = calloc(11, sizeof(char));
-		scanf("%s", s[i]);
+	    s[i] = calloc(11, sizeof(char));
+	    scanf("%s", s[i]);
 	}
 	do
 	{
-		for (int i = 0; i < n; i++)
-			printf("%s%c", s[i], i == n - 1 ? '\n' : ' ');
+	    for (int i = 0; i < n; i++)
+	    printf("%s%c", s[i], i == n - 1 ? '\n' : ' ');
 	} while (next_permutation(n, s));
+	
 	for (int i = 0; i < n; i++)
 		free(s[i]);
 	free(s);
